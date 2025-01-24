35b0c7c3d276c433447976583f6674f90b43633b
GuiHarrison <lymanzergathedigger@gmail.com>
2025-01-24 19:29:18 -0300

    Estilização 24/01/2025 19h29

--- archive-lojas.php
+++ archive-lojas.php
@@ -23,8 +23,8 @@ $args = array(
   'post_type'      => 'lojas',
   'posts_per_page' => -1,
   'post_status'    => 'publish',
-  'orderby'        => 'date',
-  'order'          => 'DESC',
+  'orderby'        => 'name',
+  'order'          => 'ASC',
 );
 
 $query = new \WP_Query( $args );
@@ -40,10 +40,11 @@ if ( $query->have_posts() ) :
   endwhile;
   wp_reset_postdata();
 endif;
-
 ?>
 
 <script>
+    var map; // Declare a variável map no escopo global
+    var markers = {};
     var requestOptions = {
         method: 'GET',
     };
@@ -51,14 +52,15 @@ endif;
     fetch("https://api.geoapify.com/v1/ipinfo?apiKey=<?php echo esc_js( $geoapify ); ?>", requestOptions)
     .then(response => response.json())
     .then(result => {
-        console.log(result);
         var latDetec = result.location.latitude || -14.235004; // Latitude padrão do Brasil
         var longDetec = result.location.longitude || -51.92528; // Longitude padrão do Brasil
         var zoom = ( latDetec === -14.235004 ) ? 6 : 13;
+        var offsetX = 0.03;
 
         window.latDetec = latDetec;
-        window.longDetec = longDetec;
+        window.longDetec = longDetec - offsetX;
         window.zoom = zoom;
+        window.offsetX = offsetX;
 
         if (typeof initMap === 'function') {
             initMap();
@@ -78,25 +80,26 @@ endif;
     function lojaSelecionada(lojaId) {
         var marker = markers[lojaId];
         if (marker) {
-            map.setZoom(16);
-            var center = marker.getPosition();
-            var offsetX = 0.003;
-            var offsetCenter = {
-                lat: center.lat(),
-                lng: center.lng() - offsetX
-            };
-            map.setCenter(offsetCenter);
-            
-            jQuery('.ll-loja').removeClass('selected');
-            jQuery('#' + lojaId).addClass('selected');
-            // openStoreInfo(store);
+          offsetX = 0.003;
+          map.setZoom(zoom + 3);
+          var center = marker.getPosition();
+          var offsetCenter = {
+              lat: center.lat(),
+              lng: center.lng() - offsetX
+          };
+          map.setCenter(offsetCenter);
+
+          jQuery('.ll-loja').removeClass('selected');
+          jQuery('#' + lojaId).addClass('selected');
+
+          jQuery('.ll-detalhes').addClass('hide-completely');
+          jQuery('#' + lojaId + ' .ll-detalhes').removeClass('hide-completely');
         }
     }
 
-    var markers = {};
     function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: zoom,
+        map = new google.maps.Map(document.getElementById('map'), { // Inicialize a variável map
+            zoom: window.zoom,
             center: {lat: window.latDetec, lng: window.longDetec},
             streetViewControl: false,
             mapTypeControl: false,
@@ -104,9 +107,9 @@ endif;
             fullscreenControl: false,
         });
 
-          var geocoder = new google.maps.Geocoder();
-          var locations = <?php echo json_encode( $locations ); ?>;
-          var icon = {
+        var geocoder = new google.maps.Geocoder();
+        var locations = <?php echo json_encode( $locations ); ?>;
+        var icon = {
             url: <?php echo '"' . esc_url( get_theme_file_uri( 'svg/icone-mapa.svg' ) ) . '"'; ?>,
             scaledSize: new google.maps.Size(36, 49),
         }
@@ -119,11 +122,11 @@ endif;
                     var location = {
                         id: store.id,
                         nome: store.title.rendered,
-                        email: store.acf.mapa_loja.email_loja,
-                        telefone: store.acf.mapa_loja.telefone_loja,
-                        whatsapp: store.acf.mapa_loja.wpp_loja,
-                        informações: store.acf.mapa_loja.infor_loja,
-                        horário: store.acf.mapa_loja.funcionamento_loja,
+                        email: store.acf.email_loja,
+                        telefone: store.acf.telefone_loja,
+                        whatsapp: store.acf.wpp_loja,
+                        informações: store.acf.infor_loja,
+                        horário: store.acf.funcionamento_loja,
                         endereço: store.acf.mapa_loja.address,
                         lat: parseFloat(store.acf.mapa_loja.lat),
                         lng: parseFloat(store.acf.mapa_loja.lng),
@@ -139,70 +142,59 @@ endif;
                     google.maps.event.addListener(marker, 'click', function() {
                         lojaSelecionada(location.id);
                     });
-                    
+
                     // Adicionar loja à lista
                     jQuery('.lista-lojas-container>.ll-lista').append(`
-                        <li class="ll-loja" id="${location.id}" onclick="lojaSelecionada(${location.id})">${location.nome}</li>
+                        <li class="ll-loja" id="${location.id}">
+                          <h4 class="ll-nome" onclick="lojaSelecionada(${location.id})">${location.nome}</h4>
+                          <p class="ll-endereco" onclick="lojaSelecionada(${location.id})">${location.endereço}</p>
+                          <div class="ll-detalhes hide-completely">
+                            <p class="ll-email">${location.email}</p>
+                            <p class="ll-telefone">${location.telefone}</p>
+                            <p class="ll-whatsapp">${location.whatsapp}</p>
+                            <p class="ll-horario">${location.horário}</p>
+                            <div class="ll-detalhes hide-completely">
+                            <a target="_blank" href="https://www.google.com/maps?saddr=My+Location&daddr=${location.lat},${location.lng}" class="ll-como-chegar">Como chegar</a>
+                            <button class="ll-fechar" onclick="zoomOut()"></button>
+                          </div>
+                          </div>
+                        </li>
                     `);
-
-                    // Remover a chamada duplicada
-                    // jQuery('.ll-loja').on('click', function() {
-                    //     var lojaClicada = jQuery(this).attr('id');
-                    //     lojaSelecionada(lojaClicada);
-                    // });
-
-                    // jQuery('.ll-loja')).forEach(function(element) {
-                    //     var marker = markers[location.id];
-
-
-                    //     element.addEventListener('click', function() {
-                    //         jQuery('.ll-loja').removeClass('selected');
-                    //         jQuery(this).addClass('selected');
-
-                    //         selectStore(this[marker]);
-                    //         map.setZoom(16);
-                    //         map.setCenter(marker.getPosition());
-                    //         // openStoreInfo(location.id);
-                    //     });
-                    // });
                 });
             })
         .catch(error => console.error('Erro ao buscar dados das lojas:', error));
     }
 
-    function openStoreInfo(store) {
-        var info = `
-            <h4 class="ll-nome">${store.title.rendered}</h4>
-            <p class="ll-endereco">${store.acf.mapa_loja.address}</p>
-            <div class="ll-detalhes">
-                <p class="ll-horario">${store.acf.mapa_loja.funcionamento_loja}</p>
-                <p class="ll-email">${store.acf.mapa_loja.email_loja}</p>
-                <p class="ll-telefone">${store.acf.mapa_loja.telefone_loja}</p>
-                <p class="ll-whatsapp">${store.acf.mapa_loja.wpp_loja}</p>
-                <a href="${store.acf.mapa_loja.maps_link}" class="ll-como-chegar">Como chegar</a>
-            </div>
-        `;
-        console.log(info);
-        // jQuery('.lista-lojas-container>.ll-lista>.ll-loja').removeClass('selected');
-        // jQuery('.lista-lojas-container>.ll-lista>.ll-loja').each(function() {
-        //     if (jQuery(this).text() === store.title.rendered) {
-        //         jQuery(this).addClass('selected');
-        //     }
-        // });
-        // jQuery('.lista-lojas-container>.ll-lista').find('.ll-detalhes').addClass('hidden');
+    function buscador_lojas() {
+      document.getElementById('busca_lojas').addEventListener('keyup', function() {
+          if (this.value.length > 3) {
+              const termoLoja = this.value.toLowerCase();
+              const $lista_loja = document.querySelectorAll('.ll-lista li');
+
+              $lista_loja.forEach(function($storeItem) {
+                  const storeName = $storeItem.textContent.toLowerCase();
+                  $storeItem.style.display = storeName.includes(termoLoja) ? '' : 'none';
+              });
+          } else {
+              const $lista_loja = document.querySelectorAll('.ll-lista li');
+              $lista_loja.forEach(function($storeItem) {
+                  $storeItem.style.display = '';
+              });
+              zoomOut();
+          }
+      });
     }
 
-        // locations.forEach(function(location) {
-        //     geocoder.geocode({'address': location['address']}, function(results, status) {
-        //       if (status === 'OK') {
-        //         new google.maps.Marker({
-        //                 map: map,
-        //                 position: results[0].geometry.location,
-        //                 icon: icon,
-        //             });
-        //         }
-        //     });
-        // });
+    function zoomOut() {
+      jQuery('.ll-detalhes').addClass('hide-completely');
+      jQuery('.ll-loja').removeClass('selected');
+      map.setCenter({lat: window.latDetec, lng: window.longDetec});
+      map.setZoom(zoom);
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        buscador_lojas();
+    });
 </script>
 
 <main class="site-main">
@@ -212,60 +204,12 @@ endif;
 			<div id="map" style="min-height: calc(100vh - 100px);">
         <p id="carregando-mapa">Carregando mapa...</p>
       </div>
+      <button id="zoom-out" onclick="zoomOut()">▣</button>
       <!-- Navegador de lojas -->
       <div class="lista-lojas-container">
-          <h3 class="ll-titulo">Encotre uma Leitura próxima de você</h3>
-          <input type="text" class="ll-campo"><button class="ll-buscar hidden">Procurar</button>
-          <ul class="ll-lista">
-              <!-- <li class="ll-loja">
-                  <a href="#_" class="ll-link"></a>
-                  <h4 class="ll-nome">Shopping Contagem</h4>
-                  <p class="ll-endereco">Avenida Severino Ballesteros Rodrigues, 850 Cabral, Contagem - MG</p>
-                  <div class="ll-detalhes hidden">
-                      <p class="ll-horario">09h00 às 18h00</p>
-                      <p class="ll-email">teste@teste</p>
-                      <p class="ll-telefone">(31) 9999 9999</p>
-                      <p class="ll-whatsapp">(31) 9 9999 9999</p>
-                      <a href="$para-o-maps" class="ll-como-chegar">Como chegar</a>
-                  </div>
-              </li>
-              <li class="ll-loja">
-                  <a href="#_" class="ll-link"></a>
-                  <h4 class="ll-nome">Shopping partage campina grande</h4>
-                  <p class="ll-endereco">Avenida Prefeito Severino Bezerra Cabral Catolé, Campina Grande - PB</p>
-                  <div class="ll-detalhes hidden">
-                      <p class="ll-horario">09h00 às 18h00</p>
-                      <p class="ll-email">teste@teste</p>
-                      <p class="ll-telefone">(31) 9999 9999</p>
-                      <p class="ll-whatsapp">(31) 9 9999 9999</p>
-                      <a href="$para-o-maps" class="ll-como-chegar">Como chegar</a>
-                  </div>
-              </li>
-              <li class="ll-loja">
-                  <a href="#_" class="ll-link"></a>
-                  <h4 class="ll-nome">Shopping Paralela</h4>
-                  <p class="ll-endereco">Av. Luís Viana, 8544 - Alphaville, Salvador - BA</p>
-                  <div class="ll-detalhes hidden">
-                      <p class="ll-horario">09h00 às 18h00</p>
-                      <p class="ll-email">teste@teste</p>
-                      <p class="ll-telefone">(31) 9999 9999</p>
-                      <p class="ll-whatsapp">(31) 9 9999 9999</p>
-                      <a href="$para-o-maps" class="ll-como-chegar">Como chegar</a>
-                  </div>
-              </li>
-              <li class="ll-loja">
-                  <a href="#_" class="ll-link"></a>
-                  <h4 class="ll-nome">IGUATEMI SÃO JOSÉ DO RIO PRETO</h4>
-                  <p class="ll-endereco">Avenida Presidente Juscelino Kubitschek de Oliveira 5000</p>
-                  <div class="ll-detalhes hidden">
-                      <p class="ll-horario">09h00 às 18h00</p>
-                      <p class="ll-email">teste@teste</p>
-                      <p class="ll-telefone">(31) 9999 9999</p>
-                      <p class="ll-whatsapp">(31) 9 9999 9999</p>
-                      <a href="$para-o-maps" class="ll-como-chegar">Como chegar</a>
-                  </div>
-              </li> -->
-          </ul>
+          <h3 class="ll-titulo">→ Encotre uma Leitura próxima de você</h3>
+          <input type="text" id="busca_lojas" class="ll-campo" placeholder="Cidade, shopping, etc…">
+          <ul class="ll-lista"></ul>
       </div>
     </div>
 	</div>
