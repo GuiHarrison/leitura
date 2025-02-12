f643a568feacb1c92e35b744cfd64dd15288e7bb
Christian <sistemanando@gmail.com>
2025-02-12 00:49:23 -0300

    Ajustado o Footer responsivo

--- footer.php
+++ footer.php
@@ -14,105 +14,89 @@ namespace Air_Light;
 
 </div><!-- #content -->
 
-<footer id="colophon" class="site-footer">
-  <div class="footer-container">
-    <!-- Logo -->
-    <div class="footer-site-branding">
-      <?php include get_theme_file_path( THEME_SETTINGS['logo'] ); ?>
-    </div>
-    <!-- Conteudo -->
-    <div class="main-area">
-      <div class="footer-column">
-        <h2>Informações</h2>
-        <?php
-          $menu_items = wp_get_nav_menu_items('informacoes');
-          if ($menu_items) {
-            echo '<ul class="footer-menu">';
-            foreach ($menu_items as $item) {
-              $texto_formatado = ucwords(strtolower($item->title));
-              echo '<li><a href="' . esc_url($item->url) . '">' . esc_html($texto_formatado) . '</a></li>';
-            }
-            echo '</ul>';
-          }
-        ?>
+  <footer id="colophon" class="site-footer">
+    <div class="footer-container">
+      <!-- Logo -->
+      <div class="footer-site-branding">
+        <?php include get_theme_file_path(THEME_SETTINGS['logo']); ?>
       </div>
-      <div class="footer-column">
-        <h2>Serviços ao cliente</h2>
-        <?php
-          $menu_name = 'servicos_ao_cliente';
-          $menu_locations = get_nav_menu_locations();
 
-          if (isset($menu_locations[$menu_name])) {
+      <!-- Conteudo -->
+      <div class="main-area">
+        <div class="footer-column">
+          <div class="title toggle-menu">
+            <picture class="arrow-icon" >
+              <source srcset="<?php echo get_theme_file_uri('/svg/arrow-black.svg'); ?>" media="(max-width: 640px)">
+              <img src="<?php echo get_theme_file_uri('/svg/arrow-blue.svg'); ?>" >
+            </picture>
+            <h2>Informações</h2>
+          </div>
+          <ul class="footer-menu">
+            <?php
+            $menu_items = wp_get_nav_menu_items('informacoes');
+            if ($menu_items) {
+              foreach ($menu_items as $item) {
+                $texto_formatado = ucwords(strtolower($item->title));
+                echo '<li><a href="' . esc_url($item->url) . '">' . esc_html($texto_formatado) . '</a></li>';
+              }
+            }
+            ?>
+          </ul>
+        </div>
+
+        <div class="footer-column">
+          <div class="title toggle-menu">
+            <picture class="arrow-icon" >
+              <source srcset="<?php echo get_theme_file_uri('/svg/arrow-black.svg'); ?>" media="(max-width: 640px)">
+              <img src="<?php echo get_theme_file_uri('/svg/arrow-blue.svg'); ?>" >
+            </picture>
+            <h2>Serviços ao cliente</h2>
+          </div>
+          <ul class="footer-menu">
+            <?php
+            $menu_name = 'servicos_ao_cliente';
+            $menu_locations = get_nav_menu_locations();
+            if (isset($menu_locations[$menu_name])) {
               $menu = wp_get_nav_menu_object($menu_locations[$menu_name]);
               $menu_items = wp_get_nav_menu_items($menu->term_id);
-
               if ($menu_items) {
-                  echo '<ul class="footer-menu">';
-                  foreach ($menu_items as $item) {
-                      $texto_formatado = ucwords(strtolower($item->title));
-                      echo '<li><a href="' . esc_url($item->url) . '">' . esc_html($texto_formatado) . '</a></li>';
-                  }
-                  echo '</ul>';
-              } else {
-                  echo '<p>Nenhum item no menu.</p>';
+                foreach ($menu_items as $item) {
+                  $texto_formatado = ucwords(strtolower($item->title));
+                  echo '<li><a href="' . esc_url($item->url) . '">' . esc_html($texto_formatado) . '</a></li>';
+                }
               }
-          } else {
-              echo '<p>O menu não foi encontrado!</p>';
-          }
-          ?>
-
-      </div>
-      <div class="footer-column">
-        <h2>Contato</h2>
-        <div class="footer-contact">
-          <div class="phone-email">
-            <p>(19) 3208-1068</p>
-            <p>(19) 3208-1068</p>
-            <p>sac@leitura.com.br</p>
-          </div>
-          <ul class="redes-sociais">
-            <li>
-              <a href="https://www.instagram.com/livrarialeitura/">
-                <span class="screen-reader-text">Instagram</span>
-              </a> 
-              <?php include get_theme_file_path( THEME_SETTINGS['Instagram'] ) ?> 
-            </li>
-            <li>
-              <a href="https://api.whatsapp.com/send?phone=5519989494465">
-                <span class="screen-reader-text">WhatsApp</span>
-              </a> 
-              <?php include get_theme_file_path( THEME_SETTINGS['WhatsApp'] ) ?> 
-            </li>
-            <li>
-              <a href="https://www.tiktok.com/@livrarialeitura">
-                <span class="screen-reader-text">TikTok</span>
-              </a>
-              <?php include get_theme_file_path( THEME_SETTINGS['TikTok'] ) ?>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/company/livraria-leitura/?originalSubdomain=br">
-                <span class="screen-reader-text">LinkedIn</span>
-              </a> 
-              <?php include get_theme_file_path( THEME_SETTINGS['LinkedIn'] ) ?>
-            </li>
-            <li>
-              <a href="https://pt-br.facebook.com/livraria.leitura">
-                <span class="screen-reader-text">Facebook</span>
-              </a>
-              <?php include get_theme_file_path( THEME_SETTINGS['Facebook'] ) ?>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/channel/UCwidZVaByjUfjsJ5KsSqurw">
-                <span class="screen-reader-text">Youtube</span>
-              </a>
-              <?php include get_theme_file_path( THEME_SETTINGS['Youtube'] ) ?>
-            </li>
+            }
+            ?>
           </ul>
+        </div>
+
+        <div class="footer-column">
+          <div class="title toggle-menu">
+            <picture class="arrow-icon" >
+              <source srcset="<?php echo get_theme_file_uri('/svg/arrow-black.svg'); ?>" media="(max-width: 640px)">
+              <img src="<?php echo get_theme_file_uri('/svg/arrow-blue.svg'); ?>" >
+            </picture>
+            <h2>Fale conosco</h2>
+          </div>
+          <div class="footer-contact">
+            <div class="phone-email">
+              <p>(19) 3208-1068</p>
+              <p>(19) 3208-1068</p>
+              <p>sac@leitura.com.br</p>
+            </div>
+            <ul class="redes-sociais">
+              <li><a href="https://www.instagram.com/livrarialeitura/"><?php include get_theme_file_path(THEME_SETTINGS['Instagram']); ?></a></li>
+              <li><a href="https://api.whatsapp.com/send?phone=5519989494465"><?php include get_theme_file_path(THEME_SETTINGS['WhatsApp']); ?></a></li>
+              <li><a href="https://www.tiktok.com/@livrarialeitura"><?php include get_theme_file_path(THEME_SETTINGS['TikTok']); ?></a></li>
+              <li><a href="https://www.linkedin.com/company/livraria-leitura/?originalSubdomain=br"><?php include get_theme_file_path(THEME_SETTINGS['LinkedIn']); ?></a></li>
+              <li><a href="https://pt-br.facebook.com/livraria.leitura"><?php include get_theme_file_path(THEME_SETTINGS['Facebook']); ?></a></li>
+              <li><a href="https://www.youtube.com/channel/UCwidZVaByjUfjsJ5KsSqurw"><?php include get_theme_file_path(THEME_SETTINGS['Youtube']); ?></a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</footer>
+  </footer>
 </div>
 
 <?php wp_footer(); ?>
@@ -127,7 +111,34 @@ namespace Air_Light;
   <span aria-hidden="true">&uarr;</span>
 </a>
 
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggleMenus = document.querySelectorAll(".toggle-menu");
 
+    toggleMenus.forEach(menu => {
+      menu.addEventListener("click", function () {
+        const menuList = this.nextElementSibling;
+        const arrow = this.querySelector(".arrow-icon");
+
+        if (menuList) {
+          // Se for "Fale Conosco", apenas a parte de telefone e e-mail será afetada
+          if (menuList.classList.contains("footer-contact")) {
+            const phoneEmail = menuList.querySelector(".phone-email");
+            if (phoneEmail) {
+              phoneEmail.classList.toggle("active");
+            }
+          } else {
+            menuList.classList.toggle("active");
+          }
+        }
+        
+        if (arrow) {
+          arrow.classList.toggle("rotated");
+        }
+      });
+    });
+  });
+</script>
 
 </body>
 </html>
